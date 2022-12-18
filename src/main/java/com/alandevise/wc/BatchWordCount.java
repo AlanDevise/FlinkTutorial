5c73b3a766fb099dfaf457a0f5fecf4d79a5e7e5
Alan Zhang <initiator@alandevise.com>
2022-12-18 12:29:10 +0800

    Initial commit

---
+++ src/main/java/com/alandevise/wc/BatchWordCount.java
@@ -0,0 +1,13 @@
+package com.alandevise.wc;
+/** 
+* @Filename: BatchWordCount.java
+* @Package: com.alandevise.wc
+* @Version: V1.0.0
+* @Description: 
+*       1. 
+* @Author: Alan Zhang [initiator@alandevise.com]
+* @Date: 2022年12月04日 14:00
+*/
+
+public class BatchWordCount {
+}
