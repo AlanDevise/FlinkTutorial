54be1cedc501251f053f63365336734fadd2f0c2
Alan Zhang <initiator@alandevise.com>
2023-01-01 17:23:56 +0800

    流式计算环境，分词计数功能实现

---
+++ src/main/java/com/alandevise/wc/StreamWordCount.java
@@ -0,0 +1,49 @@
+package com.alandevise.wc;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Filename: StreamWordCount.java
+ * @Package: com.alandevise.wc
+ * @Version: V1.0.0
+ * @Description: 1. 流式计算
+ * @Author: Alan Zhang [initiator@alandevise.com]
+ * @Date: 2023年01月01日 16:55
+ */
+
+public class StreamWordCount {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建流式执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 从参数重提取主机名和端口号
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String hostname = parameterTool.get("host");    // 主机名（主机地址）
+        int port = parameterTool.getInt("port");    // 端口号
+        // 2. 读取文本流
+        DataStreamSource<String> lineDataStream = env.socketTextStream(hostname, port);
+        // 3. 转换计算
+        SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOneTuple = lineDataStream.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            // 将一行文本以空格拆分
+            String[] words = line.split(" ");
+            // 将每个分词转换成二元组输出
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+        // 4. 分组
+        KeyedStream<Tuple2<String, Long>, String> wordAndOneKeyedStream = wordAndOneTuple.keyBy(data -> data.f0);
+        // 5. 求和
+        SingleOutputStreamOperator<Tuple2<String, Long>> sum = wordAndOneKeyedStream.sum(1);
+        // 6. 输出
+        sum.print();
+        // 7. 启动执行
+        env.execute();
+    }
+}
